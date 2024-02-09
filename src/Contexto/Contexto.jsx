82bdd86a09e19ff854cd79eac5a5319f141d2b9b
enzogit <enzo_91_10@hotmail.com>
2024-02-09 17:17:18 -0300

    primer commit solo navbar

---
+++ src/Contexto/Contexto.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contexto = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Contexto
---
+++ src/componentes/NavBar/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <header>
+        <h1>Negocio</h1>
+        
+        <nav>
+            <ul>
+                <li>Mini Cakes</li>
+                <li>Laminados</li>
+                <li>Tortas</li>
+            </ul>
+        </nav>
+
+    </header>
+  )
+}
+
+export default NavBar
