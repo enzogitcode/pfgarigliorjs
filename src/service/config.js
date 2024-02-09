da0adf4e9cb7e6d865e8f358a771253a18a30edc
enzogit <enzo_91_10@hotmail.com>
2024-02-09 18:43:03 -0300

    cambios firebase

---
+++ src/service/config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase"
+import 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBVBFXRpke3KJYTvff4PKJxE8PKQFrXFps",
+  authDomain: "mirproyectocode.firebaseapp.com",
+  projectId: "mirproyectocode",
+  storageBucket: "mirproyectocode.appspot.com",
+  messagingSenderId: "250468072450",
+  appId: "1:250468072450:web:7196687f0c739b0ee2bb1b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db= getFirestore (app);
