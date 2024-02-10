e5d038b298023930a8680073cfaf0e3e0d6c01f6
enzogit <enzo_91_10@hotmail.com>
2024-02-09 22:08:07 -0300

    cargo array

---
+++ array.js
@@ -0,0 +1,8 @@
+const productos = [
+    { id: "1", nombre: "Medialunas", precio: 450, img: "../imgs/medialunas.webp", idCat: "Laminados", stock: 50000 },
+    { id: "2", nombre: "Medialunas rellenas", precio: 500, img: "../imgs/medialunasrellenas.jpg", idCat: "Laminados", stock: 50000 },
+    { id: "1", nombre: "Pavlova", precio: 12000, img: "../imgs/pavlova.webp", idCat: "Tortas", stock: 50000 },
+    { id: "2", nombre: "Torta Oreo", precio: 14000, img: "../imgs/tortaoreo.jpg", idCat: "Tortas", stock: 50000 },
+    { id: "5", nombre: "Rogel", precio: 1100, img: "../imgs/rogel.jpg", idCat: "Tortas", stock: 50000 },
+    { id: "6", nombre: "Tiramisú", precio: 10000, img: "../imgs/tiramisu.jpg", idCat: "Tortas", stock: 50000},
+]
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import NavBar from './componentes/NavBar/NavBar'
+import CargarArray from './componentes/CargarArray/CargarArray'
 
 const App = () => {
   return (
     <div>
       <NavBar></NavBar>
-      <CargarJson/>
+      <CargarArray/>
     </div>
   )
 }
