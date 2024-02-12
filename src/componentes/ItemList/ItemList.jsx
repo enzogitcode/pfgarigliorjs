9cfe6a1059f1e2be39d0418cda236d546538f313
enzogit <enzo_91_10@hotmail.com>
2024-02-11 21:03:44 -0300

    agrego cart

--- src/componentes/ItemList/ItemList.jsx
+++ src/componentes/ItemList/ItemList.jsx
@@ -5,7 +5,7 @@ const ItemList = ({productos}) => {
   return (
     <div className="contenedorProductos">
         {productos.map(prod => <Item key={prod.id}  {...prod} />)}
-    </div>
+     </div>
   )
 }
 
