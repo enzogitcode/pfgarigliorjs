7973b6b51476eeda799272a07421b0a2d724d277
enzogit <enzo_91_10@hotmail.com>
2024-02-15 17:49:31 -0300

    se genera la orden de compra

--- src/componentes/Checkout/Checkout.jsx
+++ src/componentes/Checkout/Checkout.jsx
@@ -3,7 +3,11 @@ import { useState, useContext } from 'react'
 import { CarritoContexto } from '../../context/Context.jsx'
 import { db } from '../../services/config.js'
 import './Checkout.css'
-import { addDoc } from 'firebase/firestore'
+import { collection, addDoc } from 'firebase/firestore'
+/* import { InputGroup } from 'react-bootstrap'
+import { Form } from 'react-bootstrap'
+ */
+
 
 const Checkout = () => {
   const { carrito, vaciarCarrito, total } = useContext(CarritoContexto);
@@ -49,49 +53,66 @@ const Checkout = () => {
         vaciarCarrito();
       })
       .catch(error => {
-        console.log(error, "error al crear la compra");
+        console.log("error al crear la compra", error);
         setError("no se pudo crear la orden");
       })
   }
 
-}
 
 
-
-
-
-
-
-
-return (
-  <div>
-    <h3 id='tituloFor'>Por favor, complete el siguiente formulario para finalizar la compra</h3>
+  return (
     <div id='divFormulario'>
+      <h3>Por favor, complete el siguiente formulario para finalizar la compra</h3>
       <form id='formCheckout' onSubmit={manejadorSubmit}>
+
         {
           carrito.map(producto => (
             <div key={producto.item.id}>
               <p>{producto.item.nombre} x {producto.cantidad}</p>
-              <p>{producto.item.precio} x {producto.item.precio}</p>
+              <p>Precio: ${producto.item.precio}</p>
             </div>
           ))
         }
+         <div>
+                    <label htmlFor="nombre"> Nombre </label>
+                    <input type="text" id="nombre" onChange={(e) => setNombre(e.target.value)} />
+                </div>
 
-        <div><label htmlFor="nombre" aria-placeholder='Nombre' id='nombre' onChange={(e) => setNombre(e.target.value)}>Nombre</label><input type="text" /></div>
-        <div><label htmlFor="apellido" aria-placeholder='Apellido' id='apellido' onChange={(e) => setApellido(e.target.value)}>Apellido</label><input type="text" /></div>
-        <div><label htmlFor="telefono" aria-placeholder='Teléfono' id='telefono' onChange={(e) => setTelefono(e.target.value)}>Teléfono</label><input type="text" /></div>
-        <div><label htmlFor="email" aria-placeholder='E-mail' id='email' onChange={(e) => setEmail(e.target.value)}>E-mail</label><input type="email" /></div>
-        <div><label htmlFor="emailConf" aria-placeholder='Confirme su E-mail' id='emailConf' onChange={(e) => setEmailConfirmacion(e.target.value)}>E-mail confirmación</label><input type="email" /></div>
-        <div id="divBtnForm">
+                <div>
+                    <label htmlFor="apellido"> Apellido </label>
+                    <input type="text" id="apellido" onChange={(e) => setApellido(e.target.value)} />
+                </div>
+
+                <div>
+                    <label htmlFor="telefono"> Telefono </label>
+                    <input type="text" id="telefono" onChange={(e) => setTelefono(e.target.value)} />
+                </div>
+
+                <div>
+                    <label htmlFor="email"> E-mail </label>
+                    <input type="email" id="email" onChange={(e) => setEmail(e.target.value)} />
+                </div>
+
+                <div>
+                    <label htmlFor="emailcon"> Email Confirmación </label>
+                    <input type="email" id="emailcon" onChange={(e) => setEmailConfirmacion(e.target.value)} />
+                </div>
+
+{/* 
+        <div><label htmlFor="nombre" onChange={(e) => setNombre(e.target.value)}>Nombre</label><input id="nombre" type="text" placeholder='Nombre' /></div>
+        <div><label htmlFor="apellido" onChange={(e) => setApellido(e.target.value)}>Apellido</label><input id="apellido" type="text" placeholder='Apellido' /></div>
+        <div><label htmlFor="telefono" onChange={(e) => setApellido(e.target.value)}>Teléfono</label><input id="telefono" type="text" placeholder='Teléfono' /></div>
+        <div><label htmlFor="email" onChange={(e) => setEmail(e.target.value)}>E-mail</label><input id="email" type="email" placeholder='E-mail' /></div>
+        <div><label htmlFor="emailConfirmacion" onChange={(e) => setEmailConfirmacion(e.target.value)}>E-mail</label><input id="emailConfirmacion" type="email" placeholder='Confirme su E-mail' /></div>
+ */}        <div id="divBtnForm">
           <button className='btnForm' type='reset'>Reestablecer</button>
+          {error && <p>{error}</p>}
+          <button className='btnForm' type='submit'>Finalizar orden</button>
+          {ordenId && <strong>Gracias por su compra! Su número de compra es: {ordenId}</strong>}
         </div>
-        {error && <p>{error}</p>}
-        <button className='btnForm' type='submit'>Finalizar orden</button>
-        {ordenId && <strong>Gracias por su compra! Su número de compra es: {ordenId}</strong>}
       </form >
     </div >
-  </div >
-);
-
+  );
+}
 
 export default Checkout
