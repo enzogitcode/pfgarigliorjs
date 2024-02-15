476f5e89f3acd7b1552aec913bc92de6c3b533c2
enzogit <enzo_91_10@hotmail.com>
2024-02-15 00:05:00 -0300

    estilo checkout

--- src/componentes/Checkout/Checkout.jsx
+++ src/componentes/Checkout/Checkout.jsx
@@ -1,62 +1,36 @@
 import React from 'react'
-//import Button from 'react-bootstrap/Button';
-//import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import './Checkout.css'
 
 const Checkout = () => {
 
+
+
+
+
+
+
+
+  
   return (
-    <div id='divCheckout'>
-      <form id='formCheckout'>
-<input type="text" /><label htmlFor=""></label>    
-<input type="text" /><label htmlFor=""></label>    
-<input type="email" /><label htmlFor="">e-mail</label>    
-<input type="email" /><label htmlFor="">confirmación e-mail</label>    
-<input type="text" /><label htmlFor=""></label>
-<button type='reset'>Reestablecer</button>
-<button type='submit'>Aceptar</button>   
-      </form>
+    <div>
+      <div id='divFormulario'>
+        <h3 id='tituloFor'>Por favor, complete el siguiente formulario para finalizar la compra</h3>
+        <form id='formCheckout'>
+          <label htmlFor="nombre">Nombre</label><input type="text" />
+          <label htmlFor="apellido">Apellido</label><input type="text" />
+          <label htmlFor="teléfono">Teléfono</label><input type="text" />
+          <label htmlFor="e-mail">E-mail</label><input type="email" />
+          <label htmlFor="">Confirmación de E-mail</label><input type="email" />
+          <div id="divBtnForm">
+            <button className='btnForm' type='reset'>Reestablecer</button>
+            <button className='btnForm' type='submit'>Finalizar orden</button>
+          </div>
+        </form>
+      </div>
     </div>
-  )
- 
-
-
-
-/* 
-  return (
-    <div id='divFormulario'>
-      <Form>
-        <Form.Group className="mb-3">
-          <Form.Label>Nombre</Form.Label>
-          <Form.Control type="email" placeholder="Ingrese su email" />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Apellido</Form.Label>
-          <Form.Control type="text" placeholder="Ingrese su Apellido" />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Teléfono</Form.Label>
-          <Form.Control type="rext" placeholder="Ingrese su número de teléfono" />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>E-mail</Form.Label>
-          <Form.Control type="email" placeholder="Ingrese su email" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Por favor, confirme su E-mail</Form.Label>
-          <Form.Control type="email" placeholder="Ingrese su email" />
-        </Form.Group>
-        <Form.Group id='btnCheckoutContainer'>
-          <Button variant="primary" type="reset">
-            Reestablecer
-          </Button>
-          <Button variant="primary" type="submit">
-            Confirmar
-          </Button></Form.Group>
-      </Form>
-    </div>
-
-  ); */
+  );
 
 }
 export default Checkout
